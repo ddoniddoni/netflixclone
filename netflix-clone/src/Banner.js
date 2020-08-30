388ef546055010decb6459da6de225426bbd888f
ddoniddoni <psdkei@naver.com>
2020-08-31 04:27:43 +0900

    Update Banner Css

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./Requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -18,7 +19,9 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
     <header>
@@ -33,13 +36,18 @@ function Banner() {
         }}
       >
         <div className="banner__contents">
-          <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
           <div className="banner_buttons">
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
           </div>
-          <h1 className="banner__description">{movie?.overview}</h1>
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+          </h1>
         </div>
+        <div className="banner--fadeBottom" />
       </header>
     </header>
   );
